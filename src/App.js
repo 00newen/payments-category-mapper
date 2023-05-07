1359690e73e433e22bb195d9fa3119756aa904bc
Newen Fara <00newen@protonmail.com>
2023-05-07 23:42:04 +0200

    CLeaning boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Payments Category Mapper</h1>
       </header>
+      <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Category</th>
+            <th>Total Expenses</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Code to map over the data state and render a row for each category */}
+        </tbody>
+      </table>
+    </div>
     </div>
   );
 }
